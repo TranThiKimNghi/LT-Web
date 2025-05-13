94bf333fdfa951214db5ad7bc1bf84eeca31844d
TranThiKimNghi <kimnghi0207@gmail.com>
2025-05-13 21:16:22 +0700

    Upload thư mục Baitap3

---
+++ Baitap3/Baitap/Controllers/StudentController1.cs
@@ -0,0 +1,26 @@
+﻿using Baitap.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap.Controllers
+{
+    public class StudentController1 : Controller
+    {
+        private static List<Students> students = new List<Students>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowKQ(Students sv)
+        {
+            students.Add(sv);
+            int SL = students.Count(s => s.ChuyenNganh == sv.ChuyenNganh);
+            ViewBag.MSSV = sv.MSSV;
+            ViewBag.HoTen = sv.HoTen;
+            ViewBag.ChuyenNganh = sv.ChuyenNganh;
+            ViewBag.SoLuong = SL;
+            return View();
+        }
+    }
+}
---
+++ Baitap3/Baitap/Models/Students.cs
@@ -0,0 +1,10 @@
+﻿namespace Baitap.Models
+{
+    public class Students
+    {
+        public string MSSV { get; set; }
+        public string HoTen { get; set; }
+        public double DiemTB { get; set; }
+        public string ChuyenNganh { get; set; }
+    }
+}
